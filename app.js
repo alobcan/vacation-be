db2203f9b424225a6cc8d18b1b5d89432a24eb48
Alejandro Obando <alejandro_obando@outlook.com>
2021-03-14 23:23:46 -0400

    graphQL creates and reads values

--- app.js
+++ app.js
@@ -4,32 +4,57 @@ import { buildSchema } from 'graphql';
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
+
+const schema = buildSchema(`
+type Event {
+  _id: ID!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+}
+
+input EventInput {
+  title: String!
+  description: String!
+  price: Float!
+}
+
+type RootQuery{
+  events: [Event!]!
+}
+
+type RootMutation {
+  createEvent(eventInput: EventInput): Event
+}
+  
+schema {
+  query: RootQuery
+  mutation: RootMutation
+}
+`);
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: buildSchema(`
-      type RootQuery{
-        events: [String!]!
-      }
-
-      type RootMutation {
-        createEvent(name: String): String
-      }
-        
-      schema {
-        query: RootQuery
-        mutation: RootMutation
-      }
-    `),
+    schema: schema,
     rootValue: {
       events: () => {
-        return ['All night coding', 'React Tutorial', 'meh'];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date().toISOString(),
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
