51ff1a76676d7d9df9f6113482227dac302e4a7d
alobcano <alejandro_obando@outlook.com>
2021-06-03 18:41:47 -0400

    added comunication with FE

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -29,7 +39,7 @@ mongoose
     }
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch((err) => {
     console.log(process.env.MONGO_URL);
